6229a01b716e841116481463cb56a34ea985ecf3
Shixin Wang <swang13@mail.sfsu.edu>
2022-11-10 16:00:08 -0800

    feat: initialized Guitar instrument and visualizer and set up States

--- client/src/instruments/wanglovsym.tsx
+++ client/src/instruments/wanglovsym.tsx
@@ -1,1 +1,13 @@
 import React from 'react';
+import * as Tone from 'tone';
+
+
+import { Instrument, InstrumentProps } from '../Instruments';
+
+const Guitar: React.FC<InstrumentProps> = (props): JSX.Element => {
+    return (
+        <button>Click</button>
+    )
+}
+
+export const GuitarInstrument = new Instrument('Guitar', Guitar);
--- client/src/visualizers/wanglovesym.tsx
+++ client/src/visualizers/wanglovesym.tsx
@@ -1,1 +1,10 @@
-import React from 'react';
+import P5 from 'p5';
+import * as Tone from 'tone';
+
+import { Visualizer } from '../Visualizers';
+
+export const histogramVisualizer = new Visualizer(
+    'Histogram',
+    (p5: P5, analyzer: Tone.Analyser) => {
+    }
+)
