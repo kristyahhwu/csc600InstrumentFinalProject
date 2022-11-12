cb09f064f667a4be78b638c4e75002d7c3ca0e2a
finaldestroyer <jwunwun@gmail.com>
2022-11-12 14:41:29 -0800

    Added Kalimba Instrument to State

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -1,8 +1,10 @@
 // 3rd party
 import { List, Map } from 'immutable';
+import { KalimbaInstrument } from './instruments/Kalimba';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
+import { RainbowVisualizer } from './visualizers/Rainbow';
 import { WaveformVisualizer } from './visualizers/Waveform';
 
 
@@ -15,13 +17,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, KalimbaInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, RainbowVisualizer]);    // similar to Visualizer[]
 
 
 /**
