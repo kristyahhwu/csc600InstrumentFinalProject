8f981f6e71cb832dbf29be0ede7db024b347fa01
Shixin Wang <swang13@mail.sfsu.edu>
2022-11-13 20:35:08 -0800

    feat: push before submit

--- client/src/instruments/wanglovsym.tsx
+++ client/src/instruments/wanglovsym.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import * as Tone from 'tone';
 import img_guitar from '../img/guitar.jpg';
+import img_guitar_strings from '../img/guitar_strings.png';
 
 
 import { Instrument, InstrumentProps } from '../Instruments';
 
 const Guitar: React.FC<InstrumentProps> = (props): JSX.Element => {
     return (
-        <img src={img_guitar}></img>
+        <img src={img_guitar_strings}></img>
     )
 }
 
