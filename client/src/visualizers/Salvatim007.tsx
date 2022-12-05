7297bb7dd2efcebb7c4bec0589238c15653450c1
Salvatim007 <martysaljhost@gmail.com>
2022-12-04 19:52:20 -0800

    face

--- client/src/visualizers/Salvatim007.tsx
+++ client/src/visualizers/Salvatim007.tsx
@@ -13,21 +13,27 @@ export const ElectricVisualizer = new Visualizer(
     const height = window.innerHeight / 2;
     const dim = Math.min(width, height);
 
-    p5.background(0, 100, 0, 255);
-
-    p5.strokeWeight(dim * 0.01);
-    p5.stroke(255, 255, 255, 255);
+    p5.background(0, 100, 0, 150);
+    p5.rect(20, 20, 60, 60);//
+    p5.strokeWeight(dim * 0.05);
+    p5.stroke(50, 255, 0, 255);
     p5.noFill();
+    
+    p5.rect(250, 20, 40, 40);//
+    
 
     const values = analyzer.getValue();
     p5.beginShape();
-    for (let i = 0; i < values.length; i++) {
+    
+    for (let i = 20; i < values.length/4; i++) {
       const amplitude = values[i] as number;
-      const x = p5.map(i, 0, values.length - 1, 0, width);
+      const x = p5.map(i, 0, values.length - 5, 0, width );
       const y = height / 2 + amplitude * height;
       // Place vertex
       p5.vertex(x, y);
     }
+
+
     p5.endShape();
   },
 );
